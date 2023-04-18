b62e276f7c0c53c84abb2e109d70b541a977bd4c
Lauren Seward <lauren.unal@gmail.com>
2023-04-18 12:35:56 -0500

    Refac: update api call to async function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,22 +1,27 @@
-export const translateText = (text) => {
+export const translateText = async (text) => {
   const url = 'https://api.funtranslations.com/translate/shakespeare.json';
   const body = new URLSearchParams({
     text: text,
   });
 
-  return fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'X-FunTranslations-Api-Secret': 'ywjQCXpoYSHN_ThTKQtOvgeF',
-    },
-    body: body,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Network response was not ok: ${response.status}`);
-      }
-      return response.json();
-    })
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-FunTranslations-Api-Secret': 'ywjQCXpoYSHN_ThTKQtOvgeF',
+      },
+      body: body,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
   
