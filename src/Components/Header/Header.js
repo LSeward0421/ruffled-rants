44de5bbbc568b870121ab476251aa349f03f4be0
Lauren Seward <lauren.unal@gmail.com>
2023-04-19 18:31:06 -0500

    Style: add corrected size images and updated spacing for header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,20 @@
 import './Header.css'
 import { Link } from 'react-router-dom';
+import logo from './RuffledRantsLogo.svg'
+import historyIcon from './HistoryIcon.svg'
 
 const Header = () => {
   return (
     <div className='header'>
       <div className="logo-container">
         <Link to='/'>
-          RUFFLED RANTS
-        {/* <img src={logo} alt="Ruffled Rants Logo" className="logo" /> */}
+          <img src={logo} alt="Ruffled Rants Logo" className="logo" />
         </Link>
       </div>
       <div className="nav-link">
-        <Link to='/history'>History</Link>
+        <Link to='/history'>
+          <img src={historyIcon} alt="History Icon" className="history-icon" />
+        </Link>
       </div>
     </div>
   )
