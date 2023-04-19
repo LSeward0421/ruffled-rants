d5a032619819e1d7522efa3f4575f1632c02821d
Lauren Seward <lauren.unal@gmail.com>
2023-04-19 17:40:15 -0500

    Style: add first styling to home page and header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,18 @@
 import './Header.css'
-
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className='header'>
-      <h1>Ruffled Rants</h1>
+      <div className="logo-container">
+        <Link to='/'>
+          RUFFLED RANTS
+        {/* <img src={logo} alt="Ruffled Rants Logo" className="logo" /> */}
+        </Link>
+      </div>
+      <div className="nav-link">
+        <Link to='/history'>History</Link>
+      </div>
     </div>
   )
 }
